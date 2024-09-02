9d823e3ad05665cc53f0a1a1ec42eb59449e3ec7
Peter Huppertz <peter@dnl-core.net>
2024-09-02 23:46:50 +0200

    Add project files.

---
+++ PolyMorphismDemo-1/Dog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphismDemo_1
+{
+    public abstract class DogBase
+    {
+        public string Name { get; set; }
+
+        public DogBase(string name)
+        { 
+            this.Name = name; 
+        }
+
+        public abstract void Bark();
+        
+
+        public virtual void Pee()
+        {
+            Console.WriteLine($"{Name} is setting up to pee.");
+        }
+
+        public void JumpOnCouch()
+        {
+            Console.WriteLine($"There is now hair from {Name} all over the sofa.");
+        }
+    }
+
+    public class MaleDog : DogBase
+    {
+
+        public MaleDog(string name) : base(name) { }
+
+        public override void Bark()
+        {
+            Console.WriteLine("BARK!");
+        }
+
+        public override void Pee()
+        {
+            base.Pee();
+            Console.WriteLine("Lift leg");
+            Console.WriteLine("Aim at vertical object");
+            Console.WriteLine("Let go.");
+        }
+    }
+
+    public class FemaleDog : DogBase
+    {
+        public FemaleDog(string name) : base(name) { }
+
+        public override void Pee()
+        {
+            base.Pee();
+            Console.WriteLine("Squat");
+            Console.WriteLine("Let go.");
+        }
+
+        public override void Bark()
+        {
+            Console.WriteLine("Yip!");
+        }
+    }
+}
---
+++ PolyMorphismDemo-1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PolyMorphismDemo_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Fido = new MaleDog("Fido");
+            Fido.Bark();
+            Fido.Pee();
+            Fido.JumpOnCouch();
+
+            var Fifi = new FemaleDog("Fifi");
+            Fifi.Bark();
+            Fifi.Pee();
+            Fifi.JumpOnCouch();            
+        }
+    }
+}
